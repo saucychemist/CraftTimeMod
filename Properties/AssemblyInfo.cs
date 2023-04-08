12c668e6fc3832ddf0ab05e920e5a2e56898c170
saucychemist <130199520+saucychemist@users.noreply.github.com>
2023-04-07 23:29:16 -0400

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using System.Reflection;
+
+//This is a C# comment. Comments have no impact on compilation.
+
+[assembly: AssemblyTitle(CraftTimeMod.BuildInfo.ModName)]
+[assembly: AssemblyCopyright($"Created by {CraftTimeMod.BuildInfo.ModAuthor}")]
+
+[assembly: AssemblyVersion(CraftTimeMod.BuildInfo.ModVersion)]
+[assembly: AssemblyFileVersion(CraftTimeMod.BuildInfo.ModVersion)]
+[assembly: MelonInfo(typeof(CraftTimeMod.CraftTimeMod), CraftTimeMod.BuildInfo.ModName, CraftTimeMod.BuildInfo.ModVersion, CraftTimeMod.BuildInfo.ModAuthor)]
+
+//This tells MelonLoader that the mod is only for The Long Dark.
+[assembly: MelonGame("Hinterland", "TheLongDark")]
